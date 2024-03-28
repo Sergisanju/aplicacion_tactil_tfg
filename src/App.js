ddcff3c11e3ef67ff4de8abde884469a37a563e2
Sergisanju <sergio.sanjuas@rai.usc.es>
2024-03-28 14:03:03 +0100

    pruebas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import MostrarJSON from './MostrarJSON'; // Asegúrate de que la ruta de importación sea correcta
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <MostrarJSON /> {          
-        }
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MostrarJSON />
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import app from './firebase-config'; // Asegúrate de que la ruta de importación sea correcta
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-// Componente que muestra el contenido de un archivo JSON almacenado en Firebase Storage
-const MostrarJSON = () => {
-  const [data, setData] = useState(null); // Estado para almacenar el contenido del archivo JSON
-
-  useEffect(() => {
-    const storage = app.storage(); // Instancia de Firebase Storage
-    const pathReference = storage.ref('cartas_de_memoria/animales/animales.json'); 
-
-    pathReference.getDownloadURL()
-      .then((url) => {
-        fetch(url)
-          .then(response => response.json())
-          .then(json => {
-            setData(json); // Almacena el contenido del archivo JSON en el estado
-          })
-          .catch(error => console.error("Error al leer el JSON:", error));
-      })
-      .catch(error => {
-        console.error("Error al obtener URL:", error);
-      });
-  }, []);
-
-  return (
-    <div>
-      //Muestra el contenido del archivo JSON en un elemento si ya se ha cargado, o un mensaje de "Cargando..." si no se ha cargado
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Cargando...'} 
-    </div>
-  );
-};
-
-export default MostrarJSON;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
