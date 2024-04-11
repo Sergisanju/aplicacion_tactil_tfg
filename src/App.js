361b4c0e569fdacd61ec2b85bb4a1097534d9bde
Sergisanju <sergio.sanjuas@rai.usc.es>
2024-04-11 21:10:59 +0200

    Probando pagina inicio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { getDownloadURL, ref } from "firebase/storage";
-import storage from './firebase-config';
+import React from 'react';
+import Home from './components/home/home';
+import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fileRef = ref(storage, 'cartas_de_memoria/animales/animales.json');
-
-    getDownloadURL(fileRef)
-      .then((url) => {
-        fetch(url)
-          .then(response => response.json())
-          .then(json => setData(json))
-          .catch(error => console.error('Error al obtener datos JSON:', error));
-      })
-      .catch((error) => {
-        console.error("Error al obtener la URL del archivo:", error);
-      });
-  }, []);
-
   return (
-    <div>
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+    <div className="App">
+      <Home />
     </div>
   );
 }
---
+++ src/components/home/home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Home.css'; // Asegúrate de tener tu archivo de estilos correspondiente
+import memoryGameIcon from '../../assets/images/memory-game-icon.png';
+import categorizationGameIcon from '../../assets/images/categorization-game-icon.png';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Bienvenido a NombreAPP</h1>
+      <h2>Elige tu juego</h2>
+      <div className="games-container">
+        <div className="game memory-game">
+          <img src={memoryGameIcon} alt="Memory Game" />
+          <button>Cartas de memoria</button>
+        </div>
+        <div className="game categorization-game">
+          <img src={categorizationGameIcon} alt="Categorization Game" />
+          <button>Categorización</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
