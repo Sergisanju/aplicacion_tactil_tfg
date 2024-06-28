0c023f26587e26db0e5777c95b4507b888935b17
Sergisanju <sergio.sanjuas@rai.usc.es>
2024-06-28 13:31:09 +0200

    Seguridad Repositorio

--- backend/firebase-config.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://aplicacion-tactil-tfg-default-rtdb.europe-west1.firebasedatabase.app'
-});
-
-module.exports = admin;
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,14 +6,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD2pTtM4aLBR_e29OcKb0pTpf3wEXTM5Tc",
-  authDomain: "aplicacion-tactil-tfg.firebaseapp.com",
-  databaseURL: "https://aplicacion-tactil-tfg-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "aplicacion-tactil-tfg",
-  storageBucket: "aplicacion-tactil-tfg.appspot.com",
-  messagingSenderId: "978499274838",
-  appId: "1:978499274838:web:ac5fda7b3e65cd11ba53f7",
-  measurementId: "G-W41KBQQCWL"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializa Firebase
